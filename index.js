08e97de547695624c3d76d2f28bf16659add89d8
GitAndrewCoffee <andrew.mcneal.us@gmail.com>
2021-12-18 11:23:52 -0600

    added prompt call to index for testing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Employee = require("./lib/Employee.js")
 const inquirer = require( "inquirer" );
 
-var myEmp = Employee.newPrompt();
+var myEmp = new Employee().newPrompt();
 console.log(myEmp);
