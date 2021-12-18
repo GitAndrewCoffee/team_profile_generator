565a5df944834df3e6871aa8b6c1610dd24becc8
GitAndrewCoffee <andrew.mcneal.us@gmail.com>
2021-12-18 17:16:21 -0600

    finished prompts

--- index.js
+++ index.js
@@ -11,48 +11,86 @@ const Manager = require("./lib/Manager.js");
 //lists
 var myEmployees = [];
 
+//Prompts
+
+const internC = 'Add Intern';
+const engineerC = 'Add Engineer';
+const quitC = 'Quit';
+
+const nextStepPrompt = {
+    type: 'list',
+    name: 'nextStep',
+    message: 'What would you like to do?',
+    choices: [internC, engineerC, quitC],
+  }
+
+//
+// Functions
+//
+
+function startHere () {
+    console.log("startHere is running");
+    getEmployee("Manager");
+}
+
+function getNextStep() {
+    console.log("getNextStep is running");
+    inquirer.prompt(nextStepPrompt)
+        .then (function (data) {
+            console.log(`You chose to ${data.nextStep}`);
+            if (data.nextStep == engineerC) {
+                getEmployee("Engineer");
+            } else if (data.nextStep == internC){
+                getEmployee("Intern");
+            } else if (data.nextStep == quitC){
+                //do nothing and end the program
+            } else {
+                console.log(`${data.nextStep} is an invalid option, please choose again`);
+                getNextStep();
+            }
+        })
+}
+
 function getEmployee (empType) {
-    console.log("getEmployee is running, empType below:")
-    console.log(empType)
+
     if (empType == 'Engineer') {
-        console.log("Engineer path");
+        console.log("Enter a new Engineer Employee");
         inquirer.prompt (new Engineer().prompts)
             .then (function (data) {
-                console.log("inquire data below:")
-                console.log(data);
+
                 myEmployees.push(new Engineer(data.eName, data.eID, data.eEmail, data.eGithub)) 
-                console.log("meEmployees Array below:")
-                console.log(myEmployees);
-            })        
+
+                getNextStep();
+            })     
+
     } else if (empType == 'Intern') {
-        console.log("Intern path");
-        console.log(new Intern().prompts);
+        console.log("Enter a new Intern Employee");
+
         inquirer.prompt (new Intern().prompts)
             .then (function (data) {
-                console.log("inquire data below:")
-                console.log(data);
+
                 myEmployees.push(new Intern(data.eName, data.eID, data.eEmail, data.eSchool)) 
-                console.log("meEmployees Array below:")
-                console.log(myEmployees);
-            })        
+
+                getNextStep();
+            })     
+
     } else if (empType == 'Manager') {
-        console.log("Manager path");
+        console.log("Enter a new Manager Employee");
         inquirer.prompt (new Manager().prompts)
         .then (function (data) {
-            console.log("inquire data below:")
-            console.log(data);
+
             myEmployees.push(new Manager(data.eName, data.eID, data.eEmail, data.eSchool)) 
-            console.log("meEmployees Array below:")
-            console.log(myEmployees);
-        })        
+
+            getNextStep();
+        })     
+
     } else {
         console.log('Invalid empType passed to getEmployee: ' + empType)
     }
 }
 
-// getEmployee("Intern");
-// getEmployee("Engineer");
-// getEmployee("Manager");
-// getEmployee("BadData");
+//
+//  Application Start
+//
 
-console.log(myEmployees);
+startHere();
