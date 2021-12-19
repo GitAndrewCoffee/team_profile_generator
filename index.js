79c5d261fb21a8a9d07759a1b9c63b5c22ac5e8d
GitAndrewCoffee <andrew.mcneal.us@gmail.com>
2021-12-19 17:43:11 -0600

    added file generation

--- index.js
+++ index.js
@@ -4,9 +4,14 @@
 
 //libraries
 const inquirer = require( "inquirer" );
+const fs = require( "fs" );
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 const Manager = require("./lib/Manager.js");
+
+//templates
+const fileTop = require("./src/top.txt");
+const fileBottom = require("./src/bottom.txt");
 
 //lists
 var myEmployees = [];
@@ -43,7 +48,7 @@ function getNextStep() {
             } else if (data.nextStep == internC){
                 getEmployee("Intern");
             } else if (data.nextStep == quitC){
-                //do nothing and end the program
+                generateFile();
             } else {
                 console.log(`${data.nextStep} is an invalid option, please choose again`);
                 getNextStep();
@@ -86,6 +91,57 @@ function getEmployee (empType) {
 
     } else {
         console.log('Invalid empType passed to getEmployee: ' + empType)
+    }
+}
+
+function generateFile () {
+    console.log('generateFile is running');
+
+    //Add file start template
+    fs.writeFile("./dist/myteam.html", fileTop);
+
+    //Add Cards
+    myEmployees.forEach(function(element){
+        
+        fs.appendFile("./dist/myteam.html", buildCard(element.role, element.name, element.id, element.email, element.github));
+        
+    });
+
+    //Add file end template
+    fs.appendFile("./dist/myteam.html", fileBottom);
+}
+
+function buildCard(role, name, id, email, fourth) {
+    let fourthRow = buildFourth(role, fourth);
+
+    let cardText = `
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <div class="card-title"><h2>${name}</h2> <br> <h3>${role}</h3></div>              
+    <ul class="list-group list-group-flush">
+    <li class="list-group-item">ID: ${id}</li>
+    <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+    <li class="list-group-item">${fourthRow}</li>
+    </ul>
+    </div>
+    `
+    return cardText;
+
+}
+
+function fourthRow(role, fourth) {
+    if (role == "Manager"){
+
+        return `Office: ${fourth}`;
+
+    } else if (role == "Engineer") {
+
+        return `Github: <a href="https://github.com/${fourth}">${fourth}</a>`;
+
+    } else if (role == "Intern") {
+        
+        return `School: ${fourth}`;
+
     }
 }
 
